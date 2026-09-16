6b808240e961dee1541d93f26a61aa996d120fe1
aaliyahzm1006 <aaliyahzm.7@gmail.com>
2026-09-16 10:06:22 -0400

    Chapter 2 Exercise 1

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+int main() {
+    int num1 = 50;
+    int num2 = 100;
+
+    int total = num1 + num2;
+
+    cout << total << endl;// TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
+
+
+
+    return 0;
+    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
+}
